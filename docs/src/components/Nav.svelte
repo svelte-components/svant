71af7bb9f5f36c53d5775d9ec223e1ca922b2caa
Kujtim Hoxha <kujtimii.h@gmail.com>
2020-04-16 22:51:40 +0200

    Use layout components

--- docs/src/components/Nav.svelte
+++ docs/src/components/Nav.svelte
@@ -2,9 +2,7 @@
   <ul>
     {#each navSections as section (section.name)}
       <li>
-        <div class="group-title">
-          {section.name}
-        </div>
+        <div class="group-title">{section.name}</div>
         <ul>
           {#each section.children as link (`${section.name}-${link}`)}
             <li>
@@ -23,8 +21,8 @@
 </div>
 
 <script>
-  import navSections from '../nav.config.json';
-  import startCase from 'lodash/startCase';
+  import navSections from "../nav.config.json";
+  import startCase from "lodash/startCase";
 
   export let segment;
 
@@ -32,62 +30,64 @@
 </script>
 
 <style>
-.navigation {
-  font-family: Avenir,-apple-system,BlinkMacSystemFont,segoe ui,Roboto,helvetica neue,Arial,noto sans,sans-serif,apple color emoji,segoe ui emoji,segoe ui symbol,noto color emoji,sans-serif;
-  width: 20%; /* TODO: Change when column component is available */
-  position: fixed;
-  top: 104px; /* top nav + 40px padding */
-  bottom: 0;
-  overflow-y: scroll;
-  border-right: 1px solid #f0f0f0;
-}
+  .navigation {
+    font-family: Avenir, -apple-system, BlinkMacSystemFont, segoe ui, Roboto,
+      helvetica neue, Arial, noto sans, sans-serif, apple color emoji,
+      segoe ui emoji, segoe ui symbol, noto color emoji, sans-serif;
+    width: 16.66%; /* TODO: Change when column component is available */
+    position: fixed;
+    top: 104px; /* top nav + 40px padding */
+    bottom: 0;
+    overflow-y: scroll;
+    border-right: 1px solid #f0f0f0;
+  }
 
-.group-title {
-  height: 1.5715;
-  padding: 8px 16px 8px 40px;
-  color: rgba(0,0,0,.45);
-  font-size: 14px;
-  line-height: 1.5715;
-  margin-bottom: 16px;
-  font-size: 13px;
-}
+  .group-title {
+    height: 1.5715;
+    padding: 8px 16px 8px 40px;
+    color: rgba(0, 0, 0, 0.45);
+    font-size: 14px;
+    line-height: 1.5715;
+    margin-bottom: 16px;
+    font-size: 13px;
+  }
 
-.group-title::after {
-  position: relative;
-  top: 12px;
-  display: block;
-  width: calc(100% - 20px);
-  height: 1px;
-  background: #f0f0f0;
-  content: '';
-}
+  .group-title::after {
+    position: relative;
+    top: 12px;
+    display: block;
+    width: calc(100% - 20px);
+    height: 1px;
+    background: #f0f0f0;
+    content: "";
+  }
 
-.nav-link {
-  display: block;
-  padding: 0 16px 0 40px;
-  height: 40px;
-  margin-top: 4px;
-  margin-bottom: 8px;
-  overflow: hidden;
-  font-size: 14px;
-  line-height: 40px;
-  text-overflow: ellipsis;
-  color: rgba(0,0,0,.85);
-  text-decoration: none;
-}
+  .nav-link {
+    display: block;
+    padding: 0 16px 0 40px;
+    height: 40px;
+    margin-top: 4px;
+    margin-bottom: 8px;
+    overflow: hidden;
+    font-size: 14px;
+    line-height: 40px;
+    text-overflow: ellipsis;
+    color: rgba(0, 0, 0, 0.85);
+    text-decoration: none;
+  }
 
-.current-link {
-  color: #1890ff;
-  background-color: #e6f7ff;
-  position: relative;
-}
+  .current-link {
+    color: #1890ff;
+    background-color: #e6f7ff;
+    position: relative;
+  }
 
-.current-link::after {
-  content: "";
-  height: 100%;
-  border-right: 3px solid #1890ff;
-  position: absolute;
-  top: 0;
-  right: 0;
-}
+  .current-link::after {
+    content: "";
+    height: 100%;
+    border-right: 3px solid #1890ff;
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
 </style>
