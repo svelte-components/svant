fa1be4c9cf811242621912a06f92c09ee304b1b6
asaloff <aaron.saloff@britecore.com>
2020-05-24 16:30:50 -0400

    Select hide selected example

---
+++ docs/src/routes/select/demos/hide-selected.demo.svelte
@@ -0,0 +1,28 @@
+<Select
+  mode="multiple"
+  style="{{ width: 300 }}"
+  placeholder="Inserted are removed"
+  bind:value>
+  {#each filteredOptions as option (option.value)}
+    <Option value="{option.value}" label="{option.label}" />
+  {/each}
+</Select>
+
+<script>
+  import { Select, Option } from "svant";
+
+  const OPTIONS = [
+    { label: "Apples", value: "apples" },
+    { label: "Nails", value: "nails" },
+    { label: "Bananas", value: "bananas" },
+    { label: "Helicopters", value: "helicopters" }
+  ];
+  let value = [];
+  let filteredOptions;
+
+  $: if (value.length) {
+    filteredOptions = OPTIONS.filter(option => !value.includes(option.value));
+  } else {
+    filteredOptions = [...OPTIONS];
+  }
+</script>
